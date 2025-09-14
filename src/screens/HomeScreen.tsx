954e0b4de8d2d00210b7c6384a10aa960a888cc5
Dmytro <dmytroshot@gmail.com>
2025-09-14 18:48:48 +0300

    update home-screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,6 +3,18 @@ import { View, StyleSheet, Text, ScrollView, Image, TouchableOpacity } from 'rea
 import { Calendar } from 'react-native-calendars';
 
 export default function HomeScreen() {
+
+  const bloodData = [
+    { type: "0+", status: "low" },
+    { type: "A+", status: "high" },
+    { type: "B+", status: "low" },
+    { type: "AB+", status: "high" },
+    { type: "0-", status: "low" },
+    { type: "A-", status: "low" },
+    { type: "B-", status: "high" },
+    { type: "AB-", status: "low" },
+  ];
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
 
@@ -54,27 +66,43 @@ export default function HomeScreen() {
         />
       </View>
 
-      <Text style={styles.paragraph}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In expedita illum assumenda placeat possimus fuga eos nulla labore ea pariatur porro eum quasi a praesentium necessitatibus maxime numquam aut, ipsa aspernatur nemo? Quisquam vel accusantium praesentium dolor voluptate architecto! Rem, vel fuga sunt harum iusto voluptate culpa illo assumenda veniam mollitia, totam itaque nemo ex hic? Atque nihil error dicta eveniet voluptate, cupiditate quis. Corporis iusto laboriosam accusamus! Sapiente magnam odio at delectus corporis, error alias aperiam iusto hic sunt omnis eaque, impedit nam totam quaerat tenetur quas voluptas rem eveniet voluptatem consectetur! Veniam id quod doloribus fugit aut quia.
-      </Text>
-
       <View>
         <Text style={styles.heading}>
-          Яка кров зараз найбільш потрібна?
+          Те що може зацікавити тебе 🤭👀
         </Text>
 
-        {Array(4).fill(0).map((_, index) => (
-          <View style={styles.listItem} key={index}>
-            <Image 
-              source={require('../images/drop.png')}
-              style={{ width: 18, height: 28, marginRight: 8, resizeMode: 'contain' }}  
-            />
-            <Text style={styles.listText}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </Text>
-          </View>
-        ))}
+        <Text style={styles.paragraph}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In expedita illum assumenda placeat possimus fuga eos nulla labore ea pariatur porro eum quasi a praesentium necessitatibus maxime numquam aut, ipsa aspernatur nemo? Quisquam vel accusantium praesentium dolor voluptate architecto! Rem, vel fuga sunt harum iusto voluptate culpa illo assumenda veniam mollitia, totam itaque nemo ex hic? Atque nihil error dicta eveniet voluptate, cupiditate quis. Corporis iusto laboriosam accusamus! Sapiente magnam odio at delectus corporis, error alias aperiam iusto hic sunt omnis eaque, impedit nam totam quaerat tenetur quas voluptas rem eveniet voluptatem consectetur! Veniam id quod doloribus fugit aut quia.
+        </Text>
       </View>
+
+      <View style={styles.bloodContainer}>
+        <Text style={styles.bloodTitle}>
+          Яка кров зараз найбільш потрібна? <Text style={{ color: "red" }}>🩸</Text>
+        </Text>
+
+        <View style={styles.bloodGrid}>
+          {bloodData.map((item, index) => {
+            
+            const icon =
+              item.status === "low"
+                ? require("../images/drop_high.png")
+                : require("../images/drop_low.png");
+
+            const textColor = item.status === "low" ? "#FAFAFA" : "#2B2B2B";
+
+            return (
+              <View key={index} style={styles.bloodItem}>
+                <Image source={icon} style={styles.bloodIcon} />
+                <View style={styles.textOverlay}>
+                  <Text style={[styles.bloodLabel, { color: textColor }]}>{item.type}</Text>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+
 
       
 
@@ -84,21 +112,22 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    minHeight:"100%",
+    minHeight: "100%",
     paddingHorizontal: "5%",
     paddingVertical: 48,
-    backgroundColor: '#fff',
-    gap:20,
+    backgroundColor: "#fff",
+    gap: 20,
   },
+
   calendarWrapper: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 16,
     padding: 8,
-    borderWidth:1,
-    borderColor:"#A1A1A1",
+    borderWidth: 1,
+    borderColor: "#A1A1A1",
     marginBottom: 20,
     marginTop: 5,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 6,
     shadowOffset: { width: 0, height: 2 },
@@ -106,35 +135,85 @@ const styles = StyleSheet.create({
   },
 
   heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontFamily: "SF Pro Rounded",
+    fontWeight: "700",
+    fontStyle: "normal",
+    fontSize: 18,
+    lineHeight: 18,
+    letterSpacing: 0,
     marginBottom: 10,
-    color: '#000',
+    color: "#000",
   },
 
   paragraph: {
-    fontFamily: 'Inter',
-    fontWeight: '500',
-    lineHeight: 18, 
+    fontFamily: "Inter",
+    fontWeight: "500",
+    lineHeight: 18,
     letterSpacing: 0,
   },
+
   listItem: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    alignItems: "flex-start",
     marginBottom: 10,
   },
+
   listText: {
-    fontFamily: 'Inter',
-    fontWeight: '500',
-    lineHeight: 18, 
+    fontFamily: "Inter",
+    fontWeight: "500",
+    lineHeight: 18,
     letterSpacing: 0,
   },
 
   notificationImg: {
-    alignSelf:'flex-end',
-    width: 28, 
-    height: 28, 
-    resizeMode: 'contain',
+    alignSelf: "flex-end",
+    width: 28,
+    height: 28,
+    resizeMode: "contain",
   },
-  
+
+
+  bloodContainer: {
+    marginTop: 16,
+  },
+  bloodTitle: {
+    fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 12,
+    fontFamily: "SF Pro Rounded",
+  },
+  bloodGrid: {
+    borderRadius: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#E66A6A1A",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems:"center"
+  },
+  bloodItem: {
+    width: "22%", 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  bloodIcon: {
+    position:"relative",
+    width: 60,
+    height: 60,
+    resizeMode: "contain",
+    marginBottom: 4,
+  },
+  bloodLabel: {
+    fontSize: 14,
+    fontWeight: "500",
+    fontFamily: "Inter",
+  },
+  textOverlay: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
+
