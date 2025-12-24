dea26322a8d61b98ad934d0391a2eeb3b49d06ab
Dorosh-Max <57559932+Dorosh-Max@users.noreply.github.com>
2025-12-25 00:00:46 +0200

    SRM-52 Develop a schedule for distributing the workload of the number of people with hourly detail

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,7 +4,13 @@ import { Calendar } from 'react-native-calendars';
 import { useTheme } from "../Theme/ThemeContext";
 
 export default function HomeScreen({ navigation }) {
-  const { colors } = useTheme(); 
+  const { colors, theme, isDark: contextIsDark } = useTheme();
+  
+  const isDark = contextIsDark || theme === 'dark' || colors.text === '#FFFFFF' || colors.text === '#E0E0E0';
+
+  const logoSource = isDark 
+    ? require('../images/logo-white.png') 
+    : require('../images/logo.png');
 
   const bloodData = [
     { type: "0+", status: "low" },
@@ -16,6 +22,9 @@ export default function HomeScreen({ navigation }) {
     { type: "B-", status: "high" },
     { type: "AB-", status: "low" },
   ];
+
+  const chartData = [5, 8, 15, 25, 40, 60, 80, 60, 50, 45, 60, 70, 50, 40, 30, 25, 20, 15, 15, 10, 5, 0, 5, 0];
+  const chartLabels = ["6", "9", "12", "15", "18", "21"];
 
   const redDates = {
     "2024-08-05": { selected: true, selectedColor: "#E53935" },
@@ -44,16 +53,22 @@ export default function HomeScreen({ navigation }) {
   return (
     <ScrollView contentContainerStyle={[styles.container, { backgroundColor: colors.backgroundMain }]}>
       
-      <TouchableOpacity>
-        <Image
-          source={require('../images/notification.png')}
-          style={[styles.notificationImg, { tintColor: colors.text }]}
+      <View style={styles.headerRow}>
+        <Image 
+          source={logoSource} 
+          style={styles.logo} 
         />
-      </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require('../images/notification.png')}
+            style={[styles.notificationImg, { tintColor: colors.text }]}
+          />
+        </TouchableOpacity>
+      </View>
 
-     <View style={[styles.calendarWrapper, { backgroundColor: colors.backgroundCard, borderColor: colors.text }]}>
+      <View style={[styles.calendarWrapper, { backgroundColor: colors.backgroundCard, borderColor: colors.text }]}>
         <Calendar
-          key={colors.backgroundCard} 
+          key={colors.backgroundCard}
           current={"2024-08-01"}
           monthFormat={"MMMM"}
           enableSwipeMonths={true}
@@ -80,12 +95,9 @@ export default function HomeScreen({ navigation }) {
         <Text style={[styles.heading, { color: colors.text }]}>
           Те що може зацікавити тебе 🤭👀
         </Text>
-
         <Text style={[styles.paragraph, { color: colors.text }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </Text>
       </View>
 
@@ -93,13 +105,11 @@ export default function HomeScreen({ navigation }) {
         <Text style={[styles.bloodTitle, { color: colors.text }]}>
           Яка кров зараз найбільш потрібна? <Text style={{ color: "red" }}>🩸</Text>
         </Text>
-
         <View style={[styles.bloodGrid, { borderColor: colors.text + '1A' }]}>
           {bloodData.map((item, index) => {
             const icon = item.status === "low"
               ? require("../images/drop_high.png")
               : require("../images/drop_low.png");
-
             return (
               <View key={index} style={styles.bloodItem}>
                 <Image source={icon} style={styles.bloodIcon} />
@@ -107,6 +117,43 @@ export default function HomeScreen({ navigation }) {
               </View>
             );
           })}
+        </View>
+      </View>
+
+      <View style={styles.chartSection}>
+        <View style={styles.chartHeaderRow}>
+          <Text style={[styles.chartTitle, { color: colors.text }]}>Rush of people</Text>
+          <View style={[styles.dayBadge, { backgroundColor: colors.primary }]}>
+            <Text style={styles.dayBadgeText}>&lt; thursday</Text>
+          </View>
+        </View>
+
+        <View style={[styles.chartContainer, { backgroundColor: colors.backgroundCard, borderColor: colors.text + '1A'}]}>
+          
+          <View style={[styles.dashedLine, { borderColor: colors.text }]} />
+          <View style={styles.barsContainer}>
+            {chartData.map((height, index) => (
+              <View key={index} style={styles.barWrapper}>
+                <View
+                  style={[
+                    styles.bar,
+                    {
+                      height: `${height}%`,
+                      backgroundColor: colors.primary
+                    }
+                  ]}
+                />
+              </View>
+            ))}
+          </View>
+
+          <View style={[styles.bottomAxisLine, { backgroundColor: colors.text }]} />
+
+          <View style={styles.labelsContainer}>
+            {chartLabels.map((label, index) => (
+              <Text key={index} style={[styles.chartLabelText, { color: colors.text }]}>{label}</Text>
+            ))}
+          </View>
         </View>
       </View>
 
@@ -120,7 +167,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: "5%",
     paddingVertical: 48,
     gap: 20,
-    marginBottom: 100,
+    marginBottom: 50,
+  },
+  headerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  logo: {
+    width: 120,   
+    height: 40,    
+    resizeMode: 'contain',
   },
   calendarWrapper: {
     borderRadius: 16,
@@ -149,14 +208,13 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
   },
   notificationImg: {
-    alignSelf: "flex-end",
     width: 28,
     height: 28,
     resizeMode: "contain",
   },
   bloodContainer: {
     marginTop: 16,
-    marginBottom: 80,
+    marginBottom: 10,
   },
   bloodTitle: {
     fontSize: 16,
@@ -190,5 +248,78 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     fontFamily: "Inter",
     textAlign: 'center',
+  },
+  chartSection: {
+    marginBottom: 80,
+  },
+  chartHeaderRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  chartTitle: {
+    fontSize: 18,
+    fontWeight: "700",
+    fontFamily: "SF Pro Rounded",
+  },
+  dayBadge: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+  },
+  dayBadgeText: {
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 12,
+  },
+  chartContainer: {
+    borderRadius: 20,
+    borderWidth: 3,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    height: 200, 
+    justifyContent: 'flex-end',
+  },
+  dashedLine: {
+    width: '100%',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    opacity: 0.2,
+    marginTop: 40,
+    marginBottom: -60, 
+    zIndex: 0, 
+  },
+  barsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    height: 100, 
+    zIndex: 1, 
+    marginBottom: 5,
+  },
+  barWrapper: {
+    width: '3%',
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  bar: {
+    width: '100%',
+    borderRadius: 4,
+    minHeight: 2,
+  },
+  bottomAxisLine: {
+    width: '100%',
+    height: 1,
+    opacity: 0.1,
+    marginBottom: 8, 
+  },
+  labelsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  chartLabelText: {
+    fontSize: 10,
+    fontFamily: "Inter",
   },
 });
